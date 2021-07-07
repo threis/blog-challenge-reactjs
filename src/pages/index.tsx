0c15d31ae858e69b80c71d56034c2dc1abfd98a0
Thiago Reis <thiagoreis93@gmail.com>
2021-07-07 08:31:26 -0300

    add global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { GetStaticProps } from 'next';
 
 import Prismic from '@prismicio/client';
@@ -29,25 +30,23 @@ export default function Home({ postsPagination }: HomeProps) {
   return (
     <>
       <div className="logo">
-        <i>i</i>
-        <h1>spacetravling</h1>
-        <span>.</span>
+        <img src="/logo.svg" alt="logo" />
       </div>
       <div className="posts">
         <p className="post-title">Como utilizar Hooks</p>
         <p className="post-subtitle">Como utilizar Hooks</p>
         <div className="post-info">
           <div className="created-at">
-            <i>i</i>
+            <FiCalendar />
             <p>15 Mar 2021</p>
           </div>
           <div className="author">
-            <i>i</i>
+            <FiUser />
             <p>Joseph Oliveira</p>
           </div>
         </div>
       </div>
-      <div>Carregar mais posts</div>
+      <button className="hidden-posts">Carregar mais posts</button>
     </>
   );
 }
